949ddf79361df209c9e703d2f3235a4835652d01
CardinPatson <cardin.tiako@gmail.com>
2023-07-19 13:40:36 +0200

    Add other K8 lesson

---
+++ simple_web/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res)=>{
+  res.send("hi there")
+})
+
+app.listen(8080, ()=>{
+  console.log("listen on port 8080")
+})
