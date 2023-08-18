9f21aa43bc8dc129b686db83727bc60f12b120a5
CardinPatson <cardin.tiako@gmail.com>
2023-08-18 02:05:33 +0200

    feat(rabbit): com btw dotnet with rabbit

---
+++ rabbit-dotnet/Send/Send.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+const string message = "Hello World";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty, routingKey: "hello", basicProperties: null, body: body);
+
+Console.WriteLine($" [x] Sent {message} ");
+
+Console.WriteLine(" Press [enter] to exit ");
+Console.ReadLine();
